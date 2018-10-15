b757b13ff7bd7bea7af689d1594678ff525be00d
AnijitSau <2anijit@gmail.com>
2018-10-15 11:48:00 +0530

    code updated

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+// components 
+import Title from './components/Title';
+import Description from './components/Description';
+
+// css
+import './css/style.css'
+
+const App = () => {
+  return (
+    <div className="container">
+    	{ /* including the Title as well as Description components */ }
+  		<Title />
+  		<Description />	
+  	</div>
+  );
+};
+
+
+
+export default App;
---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+	    <span className="caption">Hello <strong>World</strong>
+	    </span>
+    </div>
+  );
+};
+
+
+export default Title;
